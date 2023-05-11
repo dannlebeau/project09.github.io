f7ceeb19a33a6d5399c90ca75880d7c673059795
Dann LeBeau <132018100+dannlebeau@users.noreply.github.com>
2023-05-11 13:36:15 -0400

    Add files via upload

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,29 +1,44 @@
-function inicializarMapa() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
-  
-        var map = L.map('mapa').setView([lat, lng], 12);
-  
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-          maxZoom: 18
-        }).addTo(map);
-  
-        var marker = L.marker([lat, lng], { draggable: true }).addTo(map);
-  
-        marker.on('dragend', function(event) {
-          var latlng = event.target.getLatLng();
-          document.getElementById('ubicacion').value = latlng.lat + ', ' + latlng.lng;
-        });
-      });
-    } else {
-      console.log('La geolocalización no es compatible con este navegador.');
-    }
+function mostrarSeccion(seccion) {
+  // Ocultar todas las secciones
+  var secciones = document.getElementsByClassName("seccion");
+  for (var i = 0; i < secciones.length; i++) {
+      secciones[i].style.display = "none";
   }
 
-  
-  //Almacenar información
+  // Mostrar la sección especificada
+  var seccionActual = document.getElementById("seccion" + seccion);
+  if (seccionActual) {
+      seccionActual.style.display = "block";
+  }
+}
 
+var botonesSiguiente = document.getElementsByClassName("boton-avanzar");
+var botonesRetroceder = document.getElementsByClassName("boton-retroceder");
 
+for (var i = 0; i < botonesSiguiente.length; i++) {
+  botonesSiguiente[i].addEventListener("click", function() {
+      var seccionActual = this.parentNode;
+      var seccionSiguiente = seccionActual.nextElementSibling;
+      while (seccionSiguiente && !seccionSiguiente.classList.contains("seccion")) {
+          seccionSiguiente = seccionSiguiente.nextElementSibling;
+      }
+      if (seccionSiguiente) {
+          seccionActual.style.display = "none";
+          seccionSiguiente.style.display = "block";
+      }
+  });
+}
+
+for (var i = 0; i < botonesRetroceder.length; i++) {
+  botonesRetroceder[i].addEventListener("click", function() {
+      var seccionActual = this.parentNode;
+      var seccionAnterior = seccionActual.previousElementSibling;
+      while (seccionAnterior && !seccionAnterior.classList.contains("seccion")) {
+          seccionAnterior = seccionAnterior.previousElementSibling;
+      }
+      if (seccionAnterior) {
+          seccionActual.style.display = "none";
+          seccionAnterior.style.display = "block";
+      }
+  });
+}
